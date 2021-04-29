1f6d896d022ab925e8f26539d308d6df6183df4e
Merefelos <annavanaga@gmail.com>
2021-04-29 20:46:38 +0300

    Initial commit: Mono default

---
+++ Hello Mono/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_Mono
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
